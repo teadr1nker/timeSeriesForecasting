b4ad1133c8241180e278e5c56fead959b259f5b0
Juhani <arsmir999@gmail.com>
2023-05-19 14:32:34 +0300

    fillna

--- file5.py
+++ file5.py
@@ -7,17 +7,30 @@ df = pd.read_excel("data/data.ods", engine="odf", sheet_name='fillna')
 df.fillna(df.median(), inplace=True)
 dfFloat = df.copy()
 df.info()
+
 for col in df.columns:
     if df[col].dtype == object:
-        print(col)
+        # print(col)
         values = df[col].unique()
         I = len(values) - 1
         for i, val in enumerate(values):
-            print(val, i / I)
+            # print(val, i / I)
             dfFloat = dfFloat.replace(val, i / I)
 
 # df.fillna(method='ffill', inplace=True)
 # print(df)
 
 dfFloat.interpolate(method='linear', limit_direction='backward', inplace=True)
-print(dfFloat)
+#print(dfFloat)
+
+
+nulls = df.isnull()
+for i in range(len(nulls)):
+    if nulls['High Value Customer'][i]:
+        if dfFloat['High Value Customer'][i] == .0:
+            df['High Value Customer'] = 'No'
+        else:
+            df['High Value Customer'] = 'Yes'
+
+print(df)
+
